87dffa0b5c0ae584efbca468afb0d60391263d3e
Dan Parry <parryd424@gmail.com>
2017-05-26 10:31:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   // your code here!
+  var challengeComments = new CommentList()
+
+  $('#note-form').on('submit', function(e){
+    e.preventDefault()
+//    debugger
+//    challengeComments.addComment(document.getElementsByTagName('input')[0].value)
+    challengeComments.addComment($('#new-comment').val())
+  //  debugger
+    $('#comment-list').html(challengeComments.render())
+    //didnt clear form but ok
+  })
 })
